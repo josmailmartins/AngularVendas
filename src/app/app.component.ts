92161912bf4a180aa75ed36c81eb50b95b5cc52d
Josmail <josmailmartins@gmail.com>
2024-10-25 23:23:49 -0400

    Criando um repositorio no Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularVendas';
+  title = 'Bem vindo ao sistema de cadastro';
 }
